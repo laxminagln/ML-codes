f55bac12c768257a788cdce21b6291144936624a
nagln <laxminagln@gmail.com>
2020-05-02 14:50:57 +0530

    Add files via upload

---
+++ cats and dogs 2.py
@@ -0,0 +1,129 @@
+import os
+import zipfile
+import random
+import tensorflow as tf
+import shutil
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from shutil import copyfile
+from os import getcwd
+
+path_cats_and_dogs = f"{getcwd()}/../tmp2/cats-and-dogs.zip"
+shutil.rmtree('/tmp')
+
+local_zip = path_cats_and_dogs
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp')
+zip_ref.close()
+
+print(len(os.listdir('/tmp/PetImages/Cat/')))
+print(len(os.listdir('/tmp/PetImages/Dog/')))
+
+try:
+    os.mkdir('/tmp/cats-v-dogs')
+    os.mkdir('/tmp/cats-v-dogs/training')
+    os.mkdir('/tmp/cats-v-dogs/testing')
+    os.mkdir('/tmp/cats-v-dogs/training/cats')
+    os.mkdir('/tmp/cats-v-dogs/training/dogs')
+    os.mkdir('/tmp/cats-v-dogs/testing/cats')
+    os.mkdir('/tmp/cats-v-dogs/testing/dogs')
+except OSError:
+    pass
+
+def split_data(SOURCE, TRAINING, TESTING, SPLIT_SIZE):
+  files=[]
+  for filename in os.listdir(SOURCE):
+    file = SOURCE + filename
+    if os.path.getsize(file) > 0:
+      files.append(filename)
+    else:
+      print('File is empty')
+
+    training_length = int(len(files) * SPLIT_SIZE)
+    testing_length = int(len(files) - training_length)
+    shuffled_set = random.sample(files, len(files))
+    training_set = shuffled_set[0:training_length]
+    testing_set = shuffled_set[-testing_length:]
+  
+  for filename in training_set:
+    src_file = SOURCE + filename
+    dest_file = TRAINING + filename
+    copyfile(src_file, dest_file)
+    
+  for filename in testing_set:
+    src_file = SOURCE + filename
+    dest_file = TESTING + filename
+    copyfile(src_file, dest_file)
+
+CAT_SOURCE_DIR = "/tmp/PetImages/Cat/"
+TRAINING_CATS_DIR = "/tmp/cats-v-dogs/training/cats/"
+TESTING_CATS_DIR = "/tmp/cats-v-dogs/testing/cats/"
+DOG_SOURCE_DIR = "/tmp/PetImages/Dog/"
+TRAINING_DOGS_DIR = "/tmp/cats-v-dogs/training/dogs/"
+TESTING_DOGS_DIR = "/tmp/cats-v-dogs/testing/dogs/"
+
+split_size = .9
+split_data(CAT_SOURCE_DIR, TRAINING_CATS_DIR, TESTING_CATS_DIR, split_size)
+split_data(DOG_SOURCE_DIR, TRAINING_DOGS_DIR, TESTING_DOGS_DIR, split_size)
+
+print(len(os.listdir('/tmp/cats-v-dogs/training/cats/')))
+print(len(os.listdir('/tmp/cats-v-dogs/training/dogs/')))
+print(len(os.listdir('/tmp/cats-v-dogs/testing/cats/')))
+print(len(os.listdir('/tmp/cats-v-dogs/testing/dogs/')))
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16,(3,3), activation = 'relu', input_shape=(150,150,3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(32,(3,3), activation = 'relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(64,(3,3), activation = 'relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation = 'relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer=RMSprop(lr=0.001), loss='binary_crossentropy', metrics=['acc'])
+
+TRAINING_DIR = '/tmp/cats-v-dogs/training/'
+train_datagen = ImageDataGenerator(rescale=1.0/255.)
+
+train_generator = train_datagen.flow_from_directory(TRAINING_DIR,
+                                                         batch_size=100,
+                                                         class_mode='binary', 
+                                                         target_size=(150,150))
+
+VALIDATION_DIR = '/tmp/cats-v-dogs/testing/'
+validation_datagen = ImageDataGenerator(rescale=1.0/255.)
+
+validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR,
+                                                         batch_size=100,
+                                                         class_mode='binary', 
+                                                         target_size=(150,150))
+
+history = model.fit_generator(train_generator,
+                              epochs=2,
+                              verbose=1,
+                              validation_data=validation_generator)
+
+%matplotlib inline
+
+import matplotlib.image  as mpimg
+import matplotlib.pyplot as plt
+
+acc=history.history['acc']
+val_acc=history.history['val_acc']
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+
+epochs=range(len(acc))
+
+plt.plot(epochs, acc, 'r', "Training Accuracy")
+plt.plot(epochs, val_acc, 'b', "Validation Accuracy")
+plt.title('Training and validation accuracy')
+plt.figure()
+
+plt.plot(epochs, loss, 'r', "Training Loss")
+plt.plot(epochs, val_loss, 'b', "Validation Loss")
+
+plt.title('Training and validation loss')
